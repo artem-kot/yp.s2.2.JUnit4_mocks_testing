4e44d41ff19f633a82f0f2c1ca30a03f3eb177a9
artem-kot <artem.alex.kot@gmail.com>
2022-06-16 23:57:26 +0400

    35% coverage reached. Parameterized test was created.

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,10 +1,38 @@
 package com.example;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
-
+@RunWith(Parameterized.class)
 public class LionTest {
+
+    private String sex;
+    private boolean expectedHasManeValue;
+    public LionTest(String sex, boolean expectedHasManeValue) {
+        this.sex = sex;
+        this.expectedHasManeValue = expectedHasManeValue;
+    }
+
+    @Parameterized.Parameters
+    public static Object[] getSexValue() {
+        return new Object[][] {
+                {"Самец", true},
+                {"Самка", false},
+                {"Небинарный лев", false}
+        };
+    }
+
+    @Test
+    public void lionSexTest() throws Exception {
+        try {
+            Lion lion = new Lion(sex, new Feline());
+            assertEquals(expectedHasManeValue, lion.hasMane);
+        } catch (Exception exception) {
+            assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
+        }
+    }
 
     @Test
     public void getKittens() {
