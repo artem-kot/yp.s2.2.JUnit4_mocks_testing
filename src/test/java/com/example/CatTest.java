47e18237b4e23771982e85f6ed3413038836edfe
artem-kot <artem.alex.kot@gmail.com>
2022-06-21 00:00:34 +0400

    Test for class CatTest is written.

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -2,15 +2,21 @@ package com.example;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class CatTest {
 
     @Test
     public void getSound() {
+        Cat cat = new Cat(new Feline());
+        assertEquals("Мяу", cat.getSound());
     }
 
     @Test
-    public void getFood() {
+    public void getFood() throws Exception {
+        Cat cat = new Cat(new Feline());
+        assertEquals(Arrays.asList("Животные", "Птицы", "Рыба"), cat.getFood());
     }
 }
